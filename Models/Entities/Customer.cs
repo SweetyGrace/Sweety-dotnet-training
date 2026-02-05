f32ae2b75f3ecf9ca83792b3d9f1af91b21b408e
sweety-grace <sweety.penumala@divami.com>
2026-02-05 10:59:03 +0530

    added column annotations

--- Models/Entities/Customer.cs
+++ Models/Entities/Customer.cs
@@ -5,8 +5,12 @@ namespace MyLearningWebApi.Models.Entities;
 [Table("customer")]
 public class Customer
 {
+    [Column("id")]
     public int Id { get; set; }
+    [Column("name")]
     public string Name { get; set; } = string.Empty;
+    [Column("email")]
     public string Email { get; set; } = string.Empty;
+    [Column("phone")]
     public string Phone { get; set; } = string.Empty;
 }
--- obj/Debug/net10.0/MyLearningWebApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyLearningWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLearningWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232bea9253e8fa94c03bacf534193419be49e3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLearningWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLearningWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
